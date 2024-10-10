bb8298f6488f1999ebf9e238c7b9e26ac092b179
RafayMehdi03 <rafaymehdi03@gmail.com>
2024-10-10 16:26:24 +0500

    Update q2.c

--- PF-Lab7/q2.c
+++ PF-Lab7/q2.c
@@ -2,8 +2,9 @@
 int main(){
     int i,limit,numA=0,numE=0,numI=0,numO=0,numU=0 ;
 	char text[50] ;
+	printf("enter your text(do not include spaces if any): ") ;
 	scanf(" %s",text) ;
-	printf("How many characters does your string has(include any spaces): ") ;
+	printf("How many characters does your string has: ") ;
 	scanf("%d",&limit) ;
 	for(i=0;i<limit;i++){
 	    switch(text[i]){
